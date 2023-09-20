2075a2f8bee16583f3ba53c4523b05254dbda261
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-09-20 16:29:39 +0200

    Created forms

--- app/Components/SectionHomePageContact.js
+++ app/Components/SectionHomePageContact.js
@@ -3,16 +3,15 @@ import Image from 'next/image';
 
 const SectionHomePage = ({ src, alt, width, height }) => {
   return (
-    <div className="w-full h-full">
+    <div className="relative w-full h-full">
       <Image src={src} alt={alt} width={width} height={height} objectFit="cover" objectPosition="center" className="w-full h-full" />
-      <div className="grid grid-rows-3 gap-4">
-        <input type="text" placeholder="E-mail"/>
-        <input type="text" placeholder="Titre"/>
-        <input type="text" placeholder="Ton message"/>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-[220px] -translate-y-1/2">
+        <input type="email" placeholder="E-mail" className="bg-white bg-opacity-90 m-2 p-2 rounded-md w-[500px]" />
+        <input type="text" placeholder="Titre" className="bg-white bg-opacity-90 m-2 p-2 rounded-md w-[500px]" />
+        <textarea id="message" name="message" className="bg-white bg-opacity-90 m-2 p-2 border rounded-md w-[500px] h-[300px]" placeholder="Ton message" required />
       </div>
     </div>
-    
-  );
+);
 }
 
 export default SectionHomePage;
