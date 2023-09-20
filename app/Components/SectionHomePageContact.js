2d3e0af0b251d29b73d0c69cd0d3e33e629d383a
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-09-20 14:43:13 +0200

    footer modified

--- app/Components/SectionHomePageContact.js
+++ app/Components/SectionHomePageContact.js
@@ -5,7 +5,13 @@ const SectionHomePage = ({ src, alt, width, height }) => {
   return (
     <div className="w-full h-full">
       <Image src={src} alt={alt} width={width} height={height} objectFit="cover" objectPosition="center" className="w-full h-full" />
+      <div className="grid grid-rows-3 gap-4">
+        <input type="text" placeholder="E-mail"/>
+        <input type="text" placeholder="Titre"/>
+        <input type="text" placeholder="Ton message"/>
+      </div>
     </div>
+    
   );
 }
 
