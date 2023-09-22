46f285c17501f5439ecc7f39a4f2612c9bdc1ac2
Fabrice <fabricevalmont1995@gmail.com>
2023-09-22 12:32:06 +0200

    SHPP trvail en cours de remise en forme avec un background

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
           <ul className ="flex items-center justify-between">
             <div className ="flex items-center">
             <Image className="mr-4" src="/New_logo_Dimension.png" alt="Logo GodsChess" width={150} height={75}/>
-            <li className="mr-4 text-white"><a href="/Gameplay">Gameplay</a></li>
-            <li className="mr-4 text-white"><a href="/Dieux">Dieux</a></li>
+            <li className="mr-4 text-white text-[24px]"><a href="/Gameplay">Gameplay</a></li>
+            <li className="mr-4 text-white text-[24px]"><a href="/Dieux">Dieux</a></li>
             </div>
             <div>
             <Image className="justify-end" src="/Image_compte_T.png" alt="Compte connection" width={100} height={50} />
--- app/Components/SectionHomePagePlay.js
+++ app/Components/SectionHomePagePlay.js
@@ -3,11 +3,10 @@ import Image from 'next/image';
 
 const SectionHomePage = ({ src, alt, width, height }) => {
   return (
-    <div className="w-full h-full">
-      <Image src={src} alt={alt} width={width} height={height} objectFit="cover" objectPosition="center" className="w-full h-full" />
-      <div className="absolute top-1/2 left-1/2 transform translate-y-[24rem] transform -translate-x-[7rem]">
-      <p className="text-white text-[75px]"> Jouer</p>
-      </div>
+    <div className="w-full h-full bg-[url('/Fond_site_web1.png')] bg-cover bg-center">
+      <Image src={src} alt={alt} width={width} height={height} objectFit="cover" objectPosition="center" className="" />
+      <Image src="/New_logo_Dimension.png" alt="Here" width={1000} height={250} objectFit="cover" objectPosition="center" className="" />
+      <p className="relative text-[75px] text-white top-1/2 m-[12rem] transform top-[17rem] left-[17rem]">Jouer</p>
     </div>
   );
 }
