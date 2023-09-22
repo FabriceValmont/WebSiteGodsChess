d47f0dba1628e4fa6a3e13bc297925e1198f5ded
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-09-22 17:24:40 +0200

    Add image dieux and add new page Gods view

--- data.js
+++ data.js
@@ -1,43 +1,71 @@
 export default [
     {
         id: 1,
-        img: "/Dieu_du_Feu_IA_-_Couleur.png",
-        name: "Dieu du Feu",
-        description: "Résumé à ajouté ici.",
-    },
-
-    {
-        id: 2,
-        img: "/Dieu_du_Feu_IA_-_Couleur.png",
-        name: "Dieu de la Glace",
-        description: "Résumé à ajouté ici.",
-    },
-
-    {
-        id: 3,
-        img: "/Dieu_du_Feu_IA_-_Couleur.png",
+        img: "/image_dieux/Déesse_de_la_Téléportation_IA_-_Couleur.png",
         name: "Déesse de la Téléportation",
         description: "Résumé à ajouté ici.",
     },
 
     {
-        id: 4,
-        img: "/Dieu_du_Feu_IA_-_Couleur.png",
-        name: "Déesse de la Foudre",
+        id: 2,
+        img: "/image_dieux/Dieu_de_la_Mort_IA_-_Couleur.png",
+        name: "Dieu de la Mort",
         description: "Résumé à ajouté ici.",
     },
 
     {
-        id: 5,
-        img: "/Dieu_du_Feu_IA_-_Couleur.png",
+        id: 3,
+        img: "/image_dieux/Dieu_de_la_Terre_IA_-_Couleur.png",
+        name: "Dieu de la Terre",
+        description: "Résumé à ajouté ici.",
+    },
+
+    {
+        id: 4,
+        img: "/image_dieux/Dieu_de_la_Secte_IA_-_Couleur.png",
         name: "Dieu de la Secte",
         description: "Résumé à ajouté ici.",
     },
 
     {
+        id: 5,
+        img: "/image_dieux/Déesse_de_l'eau_IA_-_Couleur.png",
+        name: "Déesse de l'Eau",
+        description: "Résumé à ajouté ici.",
+    },
+
+    {
         id: 6,
-        img: "/Dieu_du_Feu_IA_-_Couleur.png",
-        name: "Dieu de la Mort",
+        img: "/image_dieux/Dieu_de_la_glace_IA_-_Couleur.png",
+        name: "Dieu de la Glace",
+        description: "Résumé à ajouté ici.",
+    },
+
+    {
+        id: 7,
+        img: "/image_dieux/Déesse_de_la_Vie_IA_-_Couleur.png",
+        name: "Déesse de la Vie",
+        description: "Résumé à ajouté ici.",
+    },
+
+    {
+        id: 8,
+        img: "/image_dieux/Dieu_du_Feu_IA_-_Couleur.png",
+        name: "Dieu du Feu",
+        description: "Résumé à ajouté ici.",
+    },
+
+    {
+        id: 9,
+        img: "/image_dieux/Déesse_de_la_Foudre_IA _-_Couleur.png",
+        name: "Déesse de la Foudre",
+        description: "Résumé à ajouté ici.",
+    },
+
+    {
+        id: 10,
+        img: "/image_dieux/Déesse_du_Vent_IA_-_Couleur.png",
+        name: "Déesse du Vent",
         description: "Résumé à ajouté ici.",
     },
 ]
