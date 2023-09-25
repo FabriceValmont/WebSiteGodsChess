ae20c661cf03671965cd436a3cae2fb30a6137ae
Fabrice <fabricevalmont1995@gmail.com>
2023-09-25 20:49:15 +0200

    Use client

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,8 +1,11 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = () => {
+  
   return (
     <header className="bg-black">
         <nav>
