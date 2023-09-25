1775afd60201d040194d02eb5e6f7d78c5d85cfd
Fabrice <fabricevalmont1995@gmail.com>
2023-09-25 15:29:50 +0200

    Avancer de deux sections

--- app/Components/SectionHomePageGods.js
+++ app/Components/SectionHomePageGods.js
@@ -5,6 +5,12 @@ const SectionHomePage = () => {
   return (
     <div className="w-full h-screen flex flex-col bg-cover bg-center relative" style={{ backgroundImage: 'url(/Panorama_des_Dieux_IA.png)' }}>
       <p className="text-7xl text-black mx-auto my-14">Dieux</p>
+      <div className="text-6xl text-white flex flex-col">
+        <p className="text-4xl mx-auto mt-[14rem]"> Découvre une multitude de dieu avec chacun un pouvoir unique</p>
+      </div>
+      <div className="w-full h-screen flex flex-col bg-cover bg-center relative justify-end">
+        <button className="text-6xl text-white mb-[10rem]">Découvrir les dieux</button>
+      </div>
     </div>
   );
 }
--- app/Components/SectionHomePageMode.js
+++ app/Components/SectionHomePageMode.js
@@ -5,22 +5,22 @@ const SectionHomePage = () => {
   return (
     <div className="w-full h-screen flex flex-col bg-cover bg-center relative" style={{ backgroundImage: 'url(/GameBoardScreenWeb.png)' }}>
       <p className="text-7xl text-black mx-auto my-14">Mode</p>
-      <div className=" w-full text-5xl text-white flex justify-around items-start mx-auto">
-        <p>Clan</p>
-        <button className="text-black">Voir gameplay</button>
-        <p>Faction</p>
+      <div className=" w-full text-6xl text-white flex justify-around items-start mx-auto">
+        <p className="px-[13rem]">Clan</p>
+        <button className="text-black whitespace-normal text-3xl justify-center">Jouer au échec comme vous n'avez jamais joué avec deux nouveaux modes unique</button>
+        <p className="px-[10rem]">Faction</p>
       </div>
-      <div className=" w-full h-screen text-3xl text-white flex justify-around items-center mx-auto">
-        <div className=" flex flex-col text-center mx-auto">
-        <p className="whitespace-normal">Deux Dieux </p>
-        <p className="whitespace-normal">Deux pouvoirs unique  </p>
-        <p className="whitespace-normal px-16">Pour une bataille tactique épique </p>
+      <div className=" w-full h-screen text-3xl text-white flex justify-evenly items-center mx-auto ">
+        <div className="flex flex-col text-center mx-auto">
+          <p className="whitespace-normal pl-[6rem] pr-[4rem] ">Affronter d'autre joueur avec un pouvoir unique pour chaque clan</p>
+          <p className="whitespace-normal"></p>
+          <p className="whitespace-normal"></p>
         </div>
-        <Image src="/Plateau_d'échec_new_clan.png" alt="Plateau d'échec" width={700} height={500} objectFit="cover" objectPosition="center" className="border-8 rounded-lg border-black items-center" />
+        <div className="w-full ml-[14rem]">
+          <Image src="/Plateau_d'échec_new_clan.png" alt="Plateau d'échec" width={700} height={500} objectFit="cover" objectPosition="center" className="border-8 rounded-lg border-black"/>
+        </div>
         <div className=" flex flex-col text-center mx-auto">
-        <p className="whitespace-normal px-16">Le mode classique de GodsChess </p>
-        <p className="whitespace-normal">Mélanger 6 Dieux/Pouvoirs </p>
-        <p className="whitespace-normal">pour encore plus de combinaison </p>
+          <p className="whitespace-normal pl-[10rem] pr-[4rem]">Le mode classique de GodsChess. Passer stratégiquement au niveau supérieur en combinant le pouvoir de six dieux </p>
         </div>
       </div>
     </div>
