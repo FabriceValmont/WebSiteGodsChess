e5eae20c4feff7c6f18f60bde21375a717ee265c
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-10-08 15:21:30 +0200

    Created fetch POST in SignIn page send to server. Created method POST and query send BDS postgreSQL table users.

--- app/Components/SignIn.js
+++ app/Components/SignIn.js
@@ -1,58 +1,93 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react';
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [response, setResponse] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/inscription', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      setResponse(data); // Mettre à jour la réponse du serveur
+    } catch (error) {
+      console.error('Erreur lors de la requête POST : ', error);
+    }
+  };
+
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-            <h1 className="text-2xl font-semibold text-center text-gray-700">
-            Créer un compte
-            </h1>
-            <form className="mt-6">
-            <div className="mb-2">
-                <label
-                htmlFor="name"
-                className="block text-sm font-semibold text-gray-800"
-                >
-                Nom
-                </label>
-                <input
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-            </div>
-            <div className="mb-2">
-                <label
-                htmlFor="email"
-                className="block text-sm font-semibold text-gray-800"
-                >
-                Email
-                </label>
-                <input
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-            </div>
-            <div className="mb-2">
-                <label
-                htmlFor="password"
-                className="block text-sm font-semibold text-gray-800"
-                >
-                Mot de passe
-                </label>
-                <input
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                />
-            </div>
-            <div className="mt-6">
-                <button className="w-full px-4 py-2 text-white bg-gray-800 rounded-lg hover:bg-gray-900 focus:outline-none focus:bg-gray-600">
-                S'inscrire
-                </button>
-            </div>
-            </form>
-        </div>
+        <h1 className="text-2xl font-semibold text-center text-gray-700">
+          Créer un compte
+        </h1>
+        <form className="mt-6" onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label htmlFor="name" className="block text-sm font-semibold text-gray-800">
+              Nom
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email" className="block text-sm font-semibold text-gray-800">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="password" className="block text-sm font-semibold text-gray-800">
+              Mot de passe
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 text-white bg-gray-800 rounded-lg hover:bg-gray-900 focus:outline-none focus:bg-gray-600">
+              S'inscrire
+            </button>
+          </div>
+        </form>
+        {response && <div className="mt-4 text-green-600">{response.message}</div>}
+      </div>
     </div>
-);
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const bodyParser = require('body-parser'); // Ajout du middleware body-parser
+const cors = require('cors'); // Importez le package cors
 const {Client} = require("pg")
 
 let dataSelectionGods = []
 let dataGods = []
 
-app.get("/selectionGods", (req, res) => {
-    res.send(dataSelectionGods)
-})
-
-app.get("/Gods", (req, res) => {
-    res.send(dataGods)
-})
+app.use(cors({ origin: 'http://localhost:3000' })); // Configurez cors pour autoriser les requêtes depuis http://localhost:3000
+app.use(bodyParser.json()); // Utilisation du middleware body-parser pour analyser les données JSON
 
 // Appel de la base de donnée 
 const client = new Client ({
@@ -24,6 +21,31 @@ const client = new Client ({
 })
 
 client.connect()
+
+app.get("/selectionGods", (req, res) => {
+    res.send(dataSelectionGods)
+})
+
+app.get("/Gods", (req, res) => {
+    res.send(dataGods)
+})
+
+// Route pour gérer la soumission du formulaire POST
+app.post("/inscription", (req, res) => {
+    // Récupérez les données du corps de la requête
+    const { name, email, password } = req.body;
+  
+    // Effectuez les opérations nécessaires dans la base de données ici
+    // Exemple : insérer les données dans la base de données
+    client.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, password], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: "Erreur lors de l'inscription" });
+      } else {
+        res.status(200).json({ message: "Inscription réussie" });
+      }
+    });
+});
 
 client.query(`SELECT * FROM selection_gods`, (err, res) => {
     if(!err){
@@ -45,7 +67,7 @@ client.query(`SELECT * FROM gods`, (err, res) => {
         console.log(err.message)
         res.status(500).send("Erreur lors de la récupération des données depuis la base de données");
     }
-    client.end()
+    // client.end()
 })
 
 setTimeout(() => {
