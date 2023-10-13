0978eb91ba6a030cc4c6682e7499e54ec54e4107
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-10-13 15:03:26 +0200

    created ProfilUser and useState isLoggedIn

--- app/Login/page.js
+++ app/Login/page.js
@@ -1,23 +1,27 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
-import { Black_Ops_One} from "@next/font/google"
+import { Black_Ops_One } from "@next/font/google";
 import Login from '../Components/Login';
+import ProfilUser from '../Components/ProfilUser';
 
 const BOO = Black_Ops_One({
     subsets: ['latin'],
     weight: ['400']
-  
-  })
+});
 
 const PageLogin = () => {
-  return (
-    <div className={BOO.className}>
-        <Header/>
-        <Login />
-        <Footer/>
-    </div>
-);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <div className={BOO.className}>
+            <Header />
+            {isLoggedIn ? <ProfilUser /> : <Login />}
+            <Footer />
+        </div>
+    );
 }
 
-export default PageLogin
+export default PageLogin;
