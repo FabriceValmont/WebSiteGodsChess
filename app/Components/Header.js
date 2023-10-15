f952284c8694d2fd3fdacbfc015bfd19321a40a5
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-10-15 19:21:50 +0200

    Add hover in Header text and image connection

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -17,26 +17,26 @@ const Header = () => {
               <Link href="/">
                 <Image className="mr-4" src="/header/New_logo_Dimension.png" alt="Logo GodsChess" width={150} height={75}/>
               </Link>
-            <li className="mr-4 text-white text-[24px]">
+            <li className="mr-4 text-white text-[24px] hover:text-red-500">
               <Link href="/Gameplay">Gameplay</Link>
               </li>
-            <li className="mr-4 text-white text-[24px]">
+            <li className="mr-4 text-white text-[24px] hover:text-red-500">
               <Link href="/Gods">Dieux</Link>
             </li>
-            <li className="mr-4 text-white text-[24px]">
+            <li className="mr-4 text-white text-[24px] hover:text-red-500">
               <Link href="/Contact">Contact</Link>
             </li>
-            <li className="mr-4 text-white text-[24px]">
+            <li className="mr-4 text-white text-[24px] hover:text-red-500">
               <Link href="/Store">Magasin</Link>
             </li>
             </div>
             <div className="flex items-center">
-            <li className="mr-4 text-white text-[14px]">
+            <li className="mr-4 text-white text-[14px] hover:text-red-500">
             <Link href="/SignIn">Inscription</Link>
             </li>
             {isAuthenticated ? (
                 <Link href="/Profil"> {/* Lien vers /Profil lorsque l'utilisateur est connecté */}
-                  <Image className="justify-end" src="/header/Image_compte_T.png" alt="Compte connexion" width={100} height={50} />
+                  <Image className="justify-end" src="/homePage/News_Image.png" alt="Compte connexion" width={100} height={50} />
                 </Link>
               ) : (
                 <Link href="/Login"> {/* Lien vers /Login lorsque l'utilisateur n'est pas connecté */}
